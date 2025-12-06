e934c8daed4a9b4f5e0a4fc38357d4fc12934fe9
monisri147 <monisrimonisri208@gmail.com>
2025-12-06 11:14:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DeviceMonitor
+{
+    class TemperatureModule
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Temperature Module Running...");
+
+            VibrationModule.Run();
+        }
+    }
+
+    class VibrationModule
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Vibration Module Running...");
+        }
+    }
+}
